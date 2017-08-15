kahing/catfs

commit 4c17610ab4df255803a502ad1be020a796e17607

implement our own spawn mount so we can terminate on umount

src/main.rs
@@ -2,6 +2,7 @@
 extern crate clap;
 extern crate env_logger;
 extern crate fuse;
+extern crate libc;
 #[macro_use]
 extern crate log;
 extern crate chan_signal;
@@ -10,6 +11,7 @@ use std::error::Error;
 use std::ffi::OsStr;
 use std::path::Path;
 use std::str::FromStr;
+use std::thread;
 
 use chan_signal::Signal;
 use clap::{App, Arg};
@@ -131,9 +133,14 @@ fn main_internal() -> error::Result<()> {
     }
 
     {
-        let _session: fuse::BackgroundSession;
+        let mut session = fuse::Session::new(fs, Path::new(&flags.mount_point), &options)?;
         unsafe {
-            _session = fuse::spawn_mount(fs, &flags.mount_point, &options)?;
+            thread::spawn(move || {
+                if let Err(e) = session.run() {
+                    error!("session.run() = {}", e);
+                }
+                libc::kill(0, libc::SIGTERM);
+            });
         }
         let mut ev = evicter::Evicter::new(cache_dir, &flags.free_space);
         ev.run();

commit 466c9d92217cc2ba58a6ecf4796b11328588124b

umount if we get C-c properly
broken by 4c17610ab4df255803a502ad1be020a796e17607

src/catfs/rlibc.rs
@@ -429,6 +429,7 @@ impl File {
     }
 
     pub fn flush(&self) -> io::Result<()> {
+        debug!("flush {}", self.fd);
         // trigger a flush for the underly fd, this could be called
         // multiple times, for ex:
         //

src/main.rs
@@ -157,19 +157,69 @@ fn main_internal() -> error::Result<()> {
 
     {
         let mut session = fuse::Session::new(fs, Path::new(&flags.mount_point), &options)?;
-        unsafe {
-            thread::spawn(move || {
-                if let Err(e) = session.run() {
-                    error!("session.run() = {}", e);
-                }
-                libc::kill(0, libc::SIGTERM);
-            });
-        }
+        thread::spawn(move || {
+            if let Err(e) = session.run() {
+                error!("session.run() = {}", e);
+            }
+            unsafe { libc::kill(libc::getpid(), libc::SIGTERM) };
+        });
+
         let mut ev = evicter::Evicter::new(cache_dir, &flags.free_space);
         ev.run();
         // unmount after we get signaled becausep session will go out of scope
         signal.recv().unwrap();
+        unmount(Path::new(&flags.mount_point))?;
     }
     rlibc::close(cache_dir)?;
     return Ok(());
+}
+
+use libc::{c_char, c_int};
+use std::ffi::{CString, CStr};
+/// Unmount an arbitrary mount point
+pub fn unmount(mountpoint: &Path) -> io::Result<()> {
+    // fuse_unmount_compat22 unfortunately doesn't return a status. Additionally,
+    // it attempts to call realpath, which in turn calls into the filesystem. So
+    // if the filesystem returns an error, the unmount does not take place, with
+    // no indication of the error available to the caller. So we call unmount
+    // directly, which is what osxfuse does anyway, since we already converted
+    // to the real path when we first mounted.
+
+    #[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "dragonfly",
+                target_os = "openbsd", target_os = "bitrig", target_os = "netbsd"))]
+    #[inline]
+    fn libc_umount(mnt: &CStr) -> c_int {
+        unsafe { libc::unmount(mnt.as_ptr(), 0) }
+    }
+
+    #[cfg(not(any(target_os = "macos", target_os = "freebsd", target_os = "dragonfly",
+                      target_os = "openbsd", target_os = "bitrig", target_os = "netbsd")))]
+    #[inline]
+    fn libc_umount(mnt: &CStr) -> c_int {
+        use std::io::ErrorKind::PermissionDenied;
+
+        let rc = unsafe { libc::umount(mnt.as_ptr()) };
+        if rc < 0 && io::Error::last_os_error().kind() == PermissionDenied {
+            // Linux always returns EPERM for non-root users.  We have to let the
+            // library go through the setuid-root "fusermount -u" to unmount.
+            unsafe {
+                fuse_unmount_compat22(mnt.as_ptr());
+            }
+            0
+        } else {
+            rc
+        }
+    }
+
+    let mnt = try!(CString::new(mountpoint.as_os_str().as_bytes()));
+    let rc = libc_umount(&mnt);
+    if rc < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+extern "system" {
+    pub fn fuse_unmount_compat22(mountpoint: *const c_char);
 }